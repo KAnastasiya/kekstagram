4046212c34abcdd93e22c49949de4b6c9c2d4147
Anastasiia Kozenko <kozenko.anastasiia@gmail.com>
2016-04-15 11:49:53 +0300

    Больше модулей и событий
    Выполнила задание

--- src/upload.js
+++ src/upload.js
@@ -166,7 +166,7 @@
    * и показывается форма кадрирования.
    * @param {Event} evt
    */
-  uploadForm.onchange = function(evt) {
+  uploadForm.addEventListener('change', function(evt) {
     var element = evt.target;
     if (element.id === 'upload-file') {
       // Проверка типа загружаемого файла, тип должен быть изображением
@@ -176,7 +176,7 @@
 
         showMessage(Action.UPLOADING);
 
-        fileReader.onload = function() {
+        fileReader.addEventListener('load', function() {
           cleanupResizer();
 
           currentResizer = new Resizer(fileReader.result);
@@ -187,7 +187,7 @@
           resizeForm.classList.remove('invisible');
 
           hideMessage();
-        };
+        });
 
         fileReader.readAsDataURL(element.files[0]);
       } else {
@@ -196,18 +196,32 @@
         showMessage(Action.ERROR);
       }
     }
-  };
+  });
+
+  /**
+   * Подстановка размеров области кадрирования в поля формы кадрирования
+   */
+  window.addEventListener('resizerchange', function() {
+    var constraint = currentResizer.getConstraint();
+
+    resizeLeft.value = constraint.x;
+    resizeTop.value = constraint.y;
+    resizeSide.value = constraint.side;
+  });
 
   /**
    * Обработка ошибок заполнения полей формы кадрирования
    */
-  resizeForm.oninput = function resizeOnInput() {
+  resizeForm.addEventListener('input', function resizeOnInput() {
     var maxWidth = currentResizer._image.naturalWidth,
       maxHeight = currentResizer._image.naturalHeight,
       resizeLeftValue = +resizeLeft.value,
       resizeTopValue = +resizeTop.value,
       resizeSideValue = +resizeSide.value,
       errorMessage = '';
+
+    // Применение данных из формы кадрирования к области кадрирования
+    currentResizer.setConstraint(resizeLeftValue, resizeTopValue, resizeSideValue);
 
     if (resizeFormIsValid()) {
       resizeSubmitButton.disabled = false;
@@ -223,14 +237,14 @@
       }
       showMessage(Action.CUSTOM, errorMessage);
     }
-  };
+  });
 
   /**
    * Обработка сброса формы кадрирования. Возвращает в начальное состояние
    * и обновляет фон.
    * @param {Event} evt
    */
-  resizeForm.onreset = function(evt) {
+  resizeForm.addEventListener('reset', function(evt) {
     evt.preventDefault();
 
     cleanupResizer();
@@ -242,14 +256,14 @@
     resizeLeft.value = resizeTop.value = resizeSide.value = '';
     resizeSubmitButton.disabled = false;
     hideMessage();
-  };
+  });
 
   /**
    * Обработка отправки формы кадрирования. Если форма валидна, экспортирует
    * кропнутое изображение в форму добавления фильтра и показывает ее.
    * @param {Event} evt
    */
-  resizeForm.onsubmit = function(evt) {
+  resizeForm.addEventListener('submit', function(evt) {
     evt.preventDefault();
 
     if (resizeFormIsValid()) {
@@ -258,25 +272,25 @@
       resizeForm.classList.add('invisible');
       filterForm.classList.remove('invisible');
     }
-  };
+  });
 
   /**
    * Сброс формы фильтра. Показывает форму кадрирования.
    * @param {Event} evt
    */
-  filterForm.onreset = function(evt) {
+  filterForm.addEventListener('reset', function(evt) {
     evt.preventDefault();
 
     filterForm.classList.add('invisible');
     resizeForm.classList.remove('invisible');
-  };
+  });
 
   /**
    * Отправка формы фильтра. Возвращает в начальное состояние, предварительно
    * записав сохраненный фильтр в cookie.
    * @param {Event} evt
    */
-  filterForm.onsubmit = function(evt) {
+  filterForm.addEventListener('submit', function(evt) {
     evt.preventDefault();
 
     cleanupResizer();
@@ -284,13 +298,13 @@
 
     filterForm.classList.add('invisible');
     uploadForm.classList.remove('invisible');
-  };
+  });
 
   /**
    * Обработчик изменения фильтра. Добавляет класс из filterMap соответствующий
    * выбранному значению в форме.
    */
-  filterForm.onchange = function() {
+  filterForm.addEventListener('change', function() {
     if (!filterMap) {
       // Ленивая инициализация. Объект не создается до тех пор, пока
       // не понадобится прочитать его в первый раз, а после этого запоминается
@@ -310,7 +324,7 @@
     // убрать предыдущий примененный класс. Для этого нужно или запоминать его
     // состояние или просто перезаписывать.
     filterImage.className = 'filter-image-preview ' + filterMap[selectedFilter];
-  };
+  });
 
   cleanupResizer();
   updateBackground();

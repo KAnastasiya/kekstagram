0e5efa51df50335fe178b3814941c3911ac5fd47
Anastasiia Kozenko <kozenko.anastasiia@gmail.com>
2016-04-15 11:27:50 +0300

    Постраничный вывод и делегирование событий
    Выполнила задание. Попытка 2 (с исправлением неверной фильтрации при постраничной отрисовке)

--- src/pictures.js
+++ src/pictures.js
@@ -6,10 +6,13 @@ var picturesContainer = document.querySelector('.pictures'),
   pictureTemplate = document.querySelector('#picture-template'),
   pictureToClone,
   pictures = [],
+  pageNumber = 0,
   PICTURES_LOAD_URL = 'https://o0.github.io/assets/json/pictures.json',
   IMAGE_SIZE = 182,
   SERVER_TIMEOUT = 10000,
-  NEW_PICTURES_DELTA = 14 * 24 * 60 * 60 * 1000;
+  NEW_PICTURES_DELTA = 14 * 24 * 60 * 60 * 1000,
+  PAGE_SIZE = 12,
+  SCROLL_TIMEOUT = 100;
 
 /**
  * Поддерживаемые способы фильтрации
@@ -26,6 +29,12 @@ var filters = {
  * @type {String}
  */
 var DEFAULT_FILTER = filters.POPULAR;
+
+/**
+ * Текущий выбранный фильтр
+ * @type {String}
+ */
+var currentFilter = DEFAULT_FILTER;
 
 // Обеспечение кроссбраузерной шаблонизации
 if ('content' in pictureTemplate) {
@@ -97,17 +106,9 @@ var getPictureElement = function(data) {
 };
 
 /**
- * Отрисовка информации обо всех картинках
- * @param  {Object} picture  Информация о картинке
- */
-var renderPictures = function(picture) {
-  picturesContainer.innerHTML = '';
-  picture.forEach(getPictureElement);
-};
-
-/**
- * Получение списка картинок, загруженных за даты, более поздние, чем
- * минимально допустимая дата; сортировка этого списка по убыванию дат
+ * Фильтр "Новые". Получение списка картинок, загруженных за даты, более
+ * поздние, чем минимально допустимая дата; сортировка этого списка по
+ * убыванию дат
  * @param  {Object} picturesToFilter  Список картинок, полученный от сервера
  * @return {Object}                   Отфильтрованный список картинок
  */
@@ -129,10 +130,10 @@ var getNewPictures = function(picturesToFilter) {
 };
 
 /**
- * Сортировка картинок по убыванию количества комментариев к ним
- * @param  {Object} a  Информация об одной картинке
- * @param  {Object} b  Информация о второй картинке
- * @return {Object}    Список отсортированных картинок
+ * Фильтр "Обсуждаемые". Сортировка картинок по убыванию количества
+ * комментариев к ним
+ * @param  {Object} picturesToFilter  Список картинок, полученный от сервера
+ * @return {Object}                   Список отсортированных картинок
  */
 var getDiscussedPictures = function(picturesToFilter) {
   picturesToFilter.sort(function(a, b) {
@@ -142,7 +143,7 @@ var getDiscussedPictures = function(picturesToFilter) {
 };
 
 /**
- * Фильтрация списка картинок
+ * Фильтрация списка картинок, согласно выбранного типа
  * @param  {Object} picturesList  Исходный список картинок
  * @param  {String} filter        Примененный фильтр
  * @return {Object}               Отфильтрованный списк картинок
@@ -164,24 +165,100 @@ var getFilteredPictures = function(picturesList, filter) {
  * Обработка события нажатия на фильтры
  */
 var setFiltersEnabled = function() {
-  for (var i = 0; i < filtersList.length; i++) {
-    filtersList[i].onclick = function() {
-      setFilterEnabled(this.id);
-    };
-  }
+  filtersContainer.addEventListener('click', function(event) {
+    currentFilter = event.target.id;
+    if (event.target.classList.contains('filters-radio')) {
+      setFilterEnabled(currentFilter);
+    }
+  });
 };
 
 /**
- * Применение фильтрации согласно выбранного способа
+ * Отрисовка отфильтрованного списка картинок
  * @param {String} filter  Выбранный фильтр
  */
-var setFilterEnabled = function(filter) {
-  renderPictures( getFilteredPictures(pictures, filter) );
+var setFilterEnabled = function() {
+  pageNumber = 0;
+  renderPage( getFilteredPictures(pictures, currentFilter), true );
   for (var j = 0; j < filtersList.length; j++) {
     filtersList[j].removeAttribute('checked');
   }
-  filtersContainer.querySelector('#' + filter).setAttribute('checked', 'true');
+  filtersContainer.querySelector('#' + currentFilter).setAttribute('checked', 'true');
 };
+
+/**
+ * Проверка на наличие страниц с картинками, следующих за текущей страницей
+ * @param  {Object}  pictures  Список картинок
+ * @return {Boolean}           Признак наличия следующей страницы
+ */
+var isNextPageAvailable = function(picturesList) {
+  return pageNumber < Math.floor(picturesList.length / PAGE_SIZE);
+};
+
+/**
+ * Проверка на достижение конца (низа) текущей страницы с картинками
+ * @return {Boolean}  Признак достижения конца страницы
+ */
+var isBottomReached = function() {
+  var pageElements = document.querySelectorAll('.picture'),
+    lastPageElement = pageElements[pageElements.length - 1],
+    lastPageElementPosition = lastPageElement.getBoundingClientRect();
+
+  return lastPageElementPosition.top <= window.innerHeight;
+};
+
+/**
+ * Отрисовка следующей страницы картинок, если достигнут
+ * конец текущей страницы, при скроллинге. Применяется троттлинг
+ */
+var setScrollEnabled = function() {
+  var scrollTimeout;
+
+  window.addEventListener('scroll', function() {
+    clearTimeout(scrollTimeout);
+    scrollTimeout = setTimeout(function() {
+      renderNextPageIfNeeded( getFilteredPictures(pictures, currentFilter) );
+    }, SCROLL_TIMEOUT);
+  });
+};
+
+/**
+ * Отрисовка следующей страницы картинок при изменении размеров
+ * экрана/окна браузера
+ */
+window.addEventListener('resize', function() {
+  renderNextPageIfNeeded( getFilteredPictures(pictures, currentFilter) );
+});
+
+/**
+ * Отрисовка информации обо всех картинках текущей страницы
+ * @param {Object}  pictureList  Список картинок
+ * @param {Boolean} replace      Признак необходимости очистки
+ *                               списка ранее отрисованных картинок
+ */
+function renderPage(pictureList, replace) {
+  var from = pageNumber * PAGE_SIZE,
+    to = from + PAGE_SIZE;
+
+  if (replace) {
+    picturesContainer.innerHTML = '';
+  }
+
+  pictureList.slice(from, to).forEach(getPictureElement);
+  renderNextPageIfNeeded(pictureList);
+}
+
+/**
+ * Проверка на наличие следующей страницы с картинками и на
+ * необходимость ее отрисовки
+ * @param {Object} pictureList  Список картинок
+ */
+function renderNextPageIfNeeded(pictureList) {
+  if (isBottomReached() && isNextPageAvailable(pictureList)) {
+    pageNumber++;
+    renderPage(pictureList);
+  }
+}
 
 /**
  * Отрисовка страницы сразу после получения данных с сервера
@@ -190,5 +267,6 @@ getPictures(function(loaderPictures) {
   pictures = loaderPictures;
   setFiltersEnabled();
   setFilterEnabled(DEFAULT_FILTER);
+  setScrollEnabled();
   filtersContainer.classList.remove('hidden');
 });
